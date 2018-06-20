e456ad7f6c544c31faadfbc0039a3d65b14bf99c
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-06-19 23:42:18 -0700

    Finished styling navlinks in header

--- src/app/shared/components/tooltip/tooltipText.ts
+++ src/app/shared/components/tooltip/tooltipText.ts
@@ -11,12 +11,9 @@ export const TooltipText = {
     trends: {
         summary: {
             trendTypeButtons: {
-                rawMeasurements: 'View the actual records you\'ve created and the corresponding individual measurements along the timeline',
+                rawMeasurements: 'See the actual records you\'ve created and the corresponding individual measurements along the timeline',
                 movingAverage: 'A moving average is used to smooth out short-term fluctuations and provide insight into longer-term trends and cycles',
-                globalAverage: 'Displays a running average of all records up to that point in time. The further along in time, the more accurate the global average'
-            },
-            seriesVisibilityButtons: {
-
+                globalAverage: 'Shows a running average of all records up to that point in time. The further along in time, the more accurate the global average'
             }
         }
     }
